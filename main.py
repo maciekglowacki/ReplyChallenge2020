1691a5ab474afd0652ff363782c3fb4fe7028e07
ArturMarcinkowski <takiotaku@gmail.com>
2020-03-06 02:31:49 +0100

    added advanced map printing function

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import sys
 from CustomerHeadquarter import CustomerHeadquarter
-
-file_name = sys.argv[1]
-my_file = open(file_name, "r")
-
-map_width, map_height, customer_headquarters_count, reply_offices_max = [int(x) for x in my_file.readline().split()]
-# print(map_width, map_height, customer_headquarters_count, reply_offices_max)
-# print("XXD")
+from printing import print_map
 
 points = {
     'T': 50,
@@ -19,40 +13,22 @@ points = {
     '#': float('inf')
 }
 
+file_name = sys.argv[1]
+my_file = open(file_name, "r")
 customer_headquarters = []
+terrain = []
+map_width, map_height, customer_headquarters_count, reply_offices_max = [int(x) for x in my_file.readline().split()]
 
 for i in range(customer_headquarters_count):
     x_coord, y_coord, reward = [int(x) for x in my_file.readline().split()]
     customer_headquarters.append(CustomerHeadquarter(i, x_coord, y_coord, reward))
 
-for customer_headquarter in customer_headquarters:
-    print(customer_headquarter)
-print(points['+'])
-
-terrain = []
 for i in range(map_height):
     terrain.append([x for x in my_file.readline()])
 
-# terrain = [[] for i in range(map_height + 1)]
-# for i in range(map_height):
-#     for i2 in range(map_width):
-#         terrain[map_height].append([x for x in my_file.readline().split(' ')])
 
-# for i in range(map_width):
-#         terrain = list()  
 
- 
-
-for i in range(map_height):
-    for i2 in range(map_width):
-        print(terrain[i][i2], end = '') 
-    print(" ")   
+print_map(customer_headquarters, map_width, map_height, terrain)
 
 
 
-
-
-terrain = [[points[j] for j in my_file.readline().strip()]
-     for i in range(map_height)]
-
-print(terrain[0][1])

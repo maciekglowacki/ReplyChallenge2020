010cb7e023513f990d95fa20051fd9064b4dfc32
maciekglowacki <glowackimaciej97@gmail.com>
2020-03-06 02:34:51 +0100

    Added move functionality for object

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from CustomerHeadquarter import CustomerHeadquarter
 file_name = sys.argv[1]
 my_file = open(file_name, "r")
 
-map_width, map_height, customer_headquarters_count, reply_offices_max = [int(x) for x in my_file.readline().split()]
-# print(map_width, map_height, customer_headquarters_count, reply_offices_max)
-# print("XXD")
+map_width, map_height, customer_headquarters_count, reply_offices_max = [
+    int(x) for x in my_file.readline().split()]
+
 
 points = {
     'T': 50,
@@ -23,36 +23,58 @@ customer_headquarters = []
 
 for i in range(customer_headquarters_count):
     x_coord, y_coord, reward = [int(x) for x in my_file.readline().split()]
-    customer_headquarters.append(CustomerHeadquarter(i, x_coord, y_coord, reward))
-
-for customer_headquarter in customer_headquarters:
-    print(customer_headquarter)
-print(points['+'])
-
-terrain = []
-for i in range(map_height):
-    terrain.append([x for x in my_file.readline()])
-
-# terrain = [[] for i in range(map_height + 1)]
-# for i in range(map_height):
-#     for i2 in range(map_width):
-#         terrain[map_height].append([x for x in my_file.readline().split(' ')])
-
-# for i in range(map_width):
-#         terrain = list()  
-
- 
-
-for i in range(map_height):
-    for i2 in range(map_width):
-        print(terrain[i][i2], end = '') 
-    print(" ")   
+    customer_headquarters.append(
+        CustomerHeadquarter(i, x_coord, y_coord, reward))
 
 
+terrain = [ [j for j in my_file.readline().strip()] for i in range(map_height)]
+
+
+directions: {
+    'UP',
+    'DOWN',
+    'LEFT',
+    'RIGHT'
+}
+
+
+class Cell:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f'X: {self.x}, Y: {self.y}'
+
+    def kurwa(self, direction):
+        if direction == 'UP':
+            self.y -= 1
+        elif direction == 'DOWN':
+            self.y += 1
+        elif direction == 'LEFT':
+            self.x -= 1
+        elif direction == 'RIGHT':
+            self.x += 1
+
+
+cell = Cell(1, 2)
+print(cell)
+cell.kurwa('UP')
+print(cell)
+cell.kurwa('DOWN')
+print(cell)
+cell.kurwa('LEFT')
+print(cell)
+cell.kurwa('RIGHT')
+print(cell)
+
+def move(cell, direction):
+    result = {
+        'UP': lambda cell: cell.y - 1,
+        'DOWN': lambda cell: cell.y + 1,
+        'LEFT': lambda cell: cell.x - 1,
+        'RIGHT': lambda cell: cell.x + 1
+    }[direction](cell)
 
 
 
-terrain = [[points[j] for j in my_file.readline().strip()]
-     for i in range(map_height)]
-
-print(terrain[0][1])
